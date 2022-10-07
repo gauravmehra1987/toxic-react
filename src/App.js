fb0ea1c8654b6ca6da689b19a7c1c04b53e2cf3e
Sandeep Kaur <sandeepkaurmultani@gmail.com>
2022-10-07 22:56:37 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchAppBar from './components/common/SearchAppBar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import BasicMasonry from './components/product/BasicMasonry';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#ff0',
+      main: '#fff',
+      dark: '#000',
+      contrastText: '#2BF21E',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+        <div className="App">
+          <SearchAppBar />
+          {/* <BasicMasonry /> */}
+        </div>
+    </ThemeProvider>
+    
   );
 }
 
